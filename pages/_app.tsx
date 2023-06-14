4c8a36b1b2f6952a3f3d2d158131a56ad76c5eb6
IDSAD <idsad.ch@gmail.com>
2023-06-14 16:08:53 +0300

    tailwind setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import React, { useEffect } from 'react'
+import '../src/styles/globals.css'
 import '../src/styles/general.scss'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
