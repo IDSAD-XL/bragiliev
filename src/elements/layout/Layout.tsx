aa4cd9d96b618a5bd65593af3a84a4e9ab589a23
IDSAD <idsad.ch@gmail.com>
2023-06-16 19:52:12 +0300

    menu

--- src/elements/layout/Layout.tsx
+++ src/elements/layout/Layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import type { ReactNode } from '../../types/ReactNode'
 import Footer from './Footer'
+import Menu from './Menu'
 
 interface ILayout {
   children: ReactNode
@@ -11,6 +12,7 @@ const Layout: React.FC<ILayout> = ({ children }) => {
   return (
     <React.Fragment>
       <Header />
+      <Menu />
       <div className="h-full flex-1">{children}</div>
       <Footer />
     </React.Fragment>
