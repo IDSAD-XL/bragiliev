ecd282d88549499befd9d8c66643604f2254b6a0
Ruslan Blokhin <blokhin.rus@gmail.com>
2023-07-11 18:43:04 +0300

    add price page

---
+++ src/types/content/PricePage.ts
@@ -0,0 +1,9 @@
+import { ITitleBlock } from '../../components/elements/TitleBlock'
+import { IFiltersPriceBlock } from '../../components/elements/FiltersPriceBlock'
+import { IRegForm } from '../../components/elements/RegForm'
+
+export interface IPricePageContent {
+  titleBlock: ITitleBlock
+  filtersPriceBlock: IFiltersPriceBlock
+  regFormBlock: IRegForm
+}
