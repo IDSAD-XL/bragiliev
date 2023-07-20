b611cfc49f1bf16e8a0790f1b67863bd0bc73b17
IDSAD <idsad.ch@gmail.com>
2023-07-20 15:04:39 +0300

    fix routing

--- pages/service.tsx
+++ pages/service.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ServicePage from '../src/pages/ServicePage'
 import { serviceContent } from '../src/mock/service'
 
-const ServicesNextPage: React.FC = () => {
+const ServiceNextPage: React.FC = () => {
   return <ServicePage {...serviceContent} />
 }
 
-export default ServicesNextPage
+export default ServiceNextPage
