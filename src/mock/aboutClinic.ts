b8a101298e080038c9c7d6004ce35d6c4d801524
Ruslan Blokhin <blokhin.rus@gmail.com>
2023-07-26 16:53:28 +0300

    add AboutClinicBlock

--- src/mock/aboutClinic.ts
+++ src/mock/aboutClinic.ts
@@ -11,9 +11,19 @@ export const aboutClinicContent: IAboutClinicContent = {
     variant: 'simple',
     title: `О клинике`,
     name: '[ GRANDMED ]',
-    image: '/assets/about-clinic-bg.jpg',
-    imageMobile: '/assets/about-clinic-bg-mobile.jpg',
+    image: '/assets/about-clinic/about-clinic-bg.jpg',
+    imageMobile: '/assets/about-clinic/about-clinic-bg-mobile.jpg',
     text: 'Я веду приём более 20 лет в клинике GRANDMED, и являюсь учредителем холдинга вместе с пятью выдающимися врачами.',
+  },
+  aboutClinicBlock: {
+    variant: 'first',
+    title: 'Как клиника за 20 лет стала супербрендом',
+    name: '[ ИСТОРИЯ ]',
+    image: '/assets/about-clinic/gm-doctors.jpg',
+    imageMobile: '/assets/about-clinic/gm-doctors-mobile.jpg',
+    text: 'Сегодня «ГрандМед» – узнаваемый бренд, который уже полюбили несколько поколений пациентов.',
+    subtext:
+      '20 лет назад четыре хирурга, выпускника Военно-Медицинской Академии, – Вадим Брагилев, Алексей Украинский, Сергей Швырев, Александр Шумило — и врач-анестезиолог Михаил Альтшуллер решили основать собственную клинику, где могли бы применить свой бесценный и разнообразный опыт в сфере красоты и молодости.',
   },
   regFormBlock: {
     name: '[ Первичная консультация ]',
