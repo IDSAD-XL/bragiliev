a8711d7634cc4ea7949e283df5a4954666dac21d
Ruslan Blokhin <blokhin.rus@gmail.com>
2023-07-27 15:59:12 +0300

    naming fixes

--- src/pages/AboutClinicPage.tsx
+++ src/pages/AboutClinicPage.tsx
@@ -22,26 +22,26 @@ const AboutClinicPage: React.FC<IAboutClinic> = ({
         />
       </PageHead>
       {aboutClinicBlocks.map((item, index) => {
-        const condition: boolean = item.desc.length > 1
+        const isMoreThanOneDesc: boolean = item.desc.length > 1
 
         return (
           <AboutClinicBlock key={index} {...item}>
             <div
               className={`${
-                condition ? 'dsk:flex-col' : 'dsk:flex-row'
+                isMoreThanOneDesc ? 'dsk:flex-col' : 'dsk:flex-row'
               } mb-[30px] flex flex-[50%] flex-col gap-[30px] gap-[30px] dsk:mb-[93px] dsk:mt-[30px] dsk:justify-between`}
             >
               {!!item.text && (
                 <h5
                   className={`${
-                    condition ? 'dsk:w-[885px]' : 'dsk:w-[566px]'
+                    isMoreThanOneDesc ? 'dsk:w-[885px]' : 'dsk:w-[566px]'
                   } mt-[30px]  text-[18px] leading-[28px] dsk:mt-[0] dsk:w-[566px] dsk:min-w-[566px] dsk:text-[30px] dsk:leading-[135%] dsk:tracking-[1px]`}
                   dangerouslySetInnerHTML={{ __html: item.text }}
                 />
               )}
               <div
                 className={`${
-                  condition ? 'dsk:mt-[49px]' : ''
+                  isMoreThanOneDesc ? 'dsk:mt-[49px]' : ''
                 } flex flex-col gap-[30px] dsk:flex dsk:flex-row dsk:justify-end dsk:gap-[138px]`}
               >
                 {!!item.desc &&
