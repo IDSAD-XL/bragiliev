40bff9f642f28233b841638818c967b724a6e8b8
Alexey <alexey.okorokov.work@gmail.com>
2023-10-11 19:45:27 +0300

    fix

--- src/pages/ReviewsPage.tsx
+++ src/pages/ReviewsPage.tsx
@@ -4,8 +4,8 @@ import TitleBlock from '../components/elements/static-blocks/TitleBlock'
 import FiltersReviewsBlock from '../components/elements/statefull-components/FiltersReviewsBlock'
 import { IReviewsContent } from '../types/content/pages/ReviewsPage'
 import RegForm from '../components/elements/statefull-components/RegForm'
-import {useAppDispatch} from "../hooks/redux";
-import {openModal} from "../redux/Actions/modalActions";
+import { useAppDispatch } from '../hooks/redux'
+import { openModal } from '../redux/Actions/modalActions'
 
 export interface IReviews extends IReviewsContent {}
 
@@ -18,14 +18,14 @@ const ReviewsPage: React.FC<IReviews> = ({
   const dispatch = useAppDispatch()
 
   const leaveReviewHandle = () => {
-    openModal(dispatch, { type: 'review_form' })
+    openModal(dispatch, { type: 'review_form', content: null })
   }
 
   return (
     <Layout {...layout}>
       <TitleBlock name={infoBlock.name} title={infoBlock.title}>
         <div
-          className="button1 mt-[20px] flex h-[80px] items-center justify-center w-full md:w-[300px] dsk:mt-[0px]"
+          className="button1 mt-[20px] flex h-[80px] w-full items-center justify-center md:w-[300px] dsk:mt-[0px]"
           onClick={leaveReviewHandle}
         >
           <span className="link-plus no-underline">оставить отзыв</span>
