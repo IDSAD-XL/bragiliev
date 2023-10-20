56c1262a76d39833e30299547b22a320f427a138
Alexey <alexey.okorokov.work@gmail.com>
2023-10-20 18:48:51 +0300

    fix

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -43,11 +43,7 @@ const Footer = (): JSX.Element => {
         </div>
         <div className="text-light col-span-4 md:col-span-2 lg:col-span-1">
           <span>
-            <Link
-              href="https://grandmed.ru/images/politika_konfidencial_nosti.pdf"
-              target="_blank"
-              className="link-hover-effect"
-            >
+            <Link href="/documents" className="link-hover-effect">
               Лицензии и сертификаты
             </Link>
           </span>
