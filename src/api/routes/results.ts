76f1b28c1b324657794b1f870a20f7bb81d4117f
Alexey <idsad.ch@gmail.com>
2023-11-23 16:06:49 +0200

    changed doc id

--- src/api/routes/results.ts
+++ src/api/routes/results.ts
@@ -1,26 +1,30 @@
-import {ISelectContent} from "../../components/elements/stateless-components/Select";
-import {IResultsContent, IResultsSlide} from "../../components/elements/statefull-components/Results";
-import {resultsContent} from "../../mock/results";
-import {IResultsPage} from "../../pages/ResultsPage";
+import { ISelectContent } from '../../components/elements/stateless-components/Select'
+import {
+  IResultsContent,
+  IResultsSlide,
+} from '../../components/elements/statefull-components/Results'
+import { resultsContent } from '../../mock/results'
+import { IResultsPage } from '../../pages/ResultsPage'
+import { projectConstants } from '../../constants/projectConstants'
 
 export const getResults = async (): Promise<IResultsPage> => {
   try {
     interface resultsDTO {
-      selects: ISelectContent[],
+      selects: ISelectContent[]
       results: IResultsSlide[]
     }
 
-    const fetchUrl = 'https://grandmed.ru/ajax/api/result.php?id=9110'
+    const fetchUrl = `https://grandmed.ru/ajax/api/result.php?id=${projectConstants.doctorId}`
     const resp = await fetch(fetchUrl)
     const fetchData: resultsDTO = await resp.json()
 
     const resultsData: IResultsContent = {
       ...resultsContent.resultsBlock,
       results: fetchData?.results,
-      selects: fetchData?.selects
+      selects: fetchData?.selects,
     }
 
-    return  {...resultsContent, resultsBlock: resultsData}
+    return { ...resultsContent, resultsBlock: resultsData }
   } catch (e) {
     return resultsContent
   }
--- src/api/routes/reviews.ts
+++ src/api/routes/reviews.ts
@@ -1,14 +1,21 @@
-import {reviewsContent} from "../../mock/reviews";
-import {IReviews} from "../../pages/ReviewsPage";
-import {IReviewsFiltersItem} from "../../components/elements/statefull-components/FiltersReviewsBlock";
+import { reviewsContent } from '../../mock/reviews'
+import { IReviews } from '../../pages/ReviewsPage'
+import { IReviewsFiltersItem } from '../../components/elements/statefull-components/FiltersReviewsBlock'
+import { projectConstants } from '../../constants/projectConstants'
 
 export const getReviews = async (): Promise<IReviews> => {
   try {
-    const fetchUrl = 'https://grandmed.ru/ajax/api/reviews.php?id=9110'
+    const fetchUrl = `https://grandmed.ru/ajax/api/reviews.php?id=${projectConstants.doctorId}`
     const resp = await fetch(fetchUrl)
     const fetchData: IReviewsFiltersItem[] = await resp.json()
 
-    return  {...reviewsContent, filtersReviewsBlock: { selects: reviewsContent.filtersReviewsBlock.selects, slides: fetchData }}
+    return {
+      ...reviewsContent,
+      filtersReviewsBlock: {
+        selects: reviewsContent.filtersReviewsBlock.selects,
+        slides: fetchData,
+      },
+    }
   } catch (e) {
     return reviewsContent
   }
